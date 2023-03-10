dcabc4c86dc12817802bf2ae5c9ad417a78bae23
AlexStorm <info@alexstorm.ru>
2023-03-10 12:23:05 +0530

    fix conflicts

--- src/lib/crypto-tools.ts
+++ src/lib/crypto-tools.ts
@@ -1,6 +1,14 @@
 import * as bip39 from 'bip39';
+import * as jwt from 'jsonwebtoken';
+
+
 
 
 export function generateMnemonic() {
     return bip39.generateMnemonic();
+}
+
+export function genJwt(payload:any) {
+    const privateKey = 'your_private_key_here';
+    const token = jwt.sign(payload, privateKey, {algorithm: 'RS256'});
 }
