6c3da44eff1012a5d4470179bbb333f32760bf71
AlexStorm <info@alexstorm.ru>
2023-05-06 05:03:03 +0530

    auth time fixes

--- src/lib/crypto-tools.ts
+++ src/lib/crypto-tools.ts
@@ -3,19 +3,20 @@
 import * as secp256k1 from 'secp256k1';
 import {sha256} from "@cosmjs/crypto";
 import {entropyToMnemonic} from "@cosmjs/crypto/build/bip39";
+import {buf2hex} from "./tokens";
 
 
-export class CryptoTools{
+export class CryptoTools {
     encoder = new TextEncoder();
 
     async privateKeyFromSeed(seedPhrase: string): Promise<Uint8Array> {
-        const hash:ArrayBuffer = await sha256( this.encoder.encode(seedPhrase) );
+        const hash: ArrayBuffer = await sha256(this.encoder.encode(seedPhrase));
         const privateKey = new Uint8Array(hash.slice(0, 32));
-        const hexHash = Array.from(new Uint8Array( hash)).map(b => b.toString(16).padStart(2, '0')).join('');
-        console.log("HASH",hexHash)
+        const hexHash = buf2hex(new Uint8Array(hash));
+        console.log("HASH", hexHash)
         while (!secp256k1.privateKeyVerify(privateKey)) {
             const reHash = await sha256(new Uint8Array(hash));
-            privateKey.set( new Uint8Array(reHash.slice(0, 32)));
+            privateKey.set(new Uint8Array(reHash.slice(0, 32)));
         }
         return privateKey;
     }
@@ -24,8 +25,9 @@ export class CryptoTools{
         return secp256k1.publicKeyCreate(privateKey);
     }
 }
+
 export function generateMnemonic() {
-    const entropy =new Uint8Array(32)
+    const entropy = new Uint8Array(32)
     window.crypto.getRandomValues(entropy);
-    return   entropyToMnemonic(entropy)
+    return entropyToMnemonic(entropy)
 }
